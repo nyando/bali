714656ba93a2b12e56ff5602b9265978fa3ff3f1
Nicki Dorrmann <nicki.dorrmann@googlemail.com>
2022-04-15 13:29:46 +0200

    added intermediate registers to LVA

--- design/control/arrayblock.sv
+++ design/control/arrayblock.sv
@@ -14,15 +14,20 @@ module arrayblock #(
     output done
 );
 
+    logic [31:0] read_out;
+    logic [31:0] write_in;
+    logic [ADDR - 1:0] mem_addr;
+    logic writing;
+
     block_ram #(
         .DATA(32),
         .SIZE(ARR_SIZE)
     ) arrmem (
         .clk(clk),
-        .write_enable(write),
-        .data(writevalue),
-        .addr(addr),
-        .data_out(readvalue)
+        .write_enable(writing),
+        .data(write_in),
+        .addr(mem_addr),
+        .data_out(read_out)
     );
 
     const logic [1:0] IDLE  = 2'b00;
@@ -34,11 +39,43 @@ module arrayblock #(
     logic is_done;
 
     always @ (posedge clk) begin
+        
         if (rst) begin
             state <= IDLE;
+            write_in <= 32'h0000_0000;
+            writing <= 0;
+            is_done <= 0;
         end
 
         case (state)
+            IDLE: begin
+                writing <= 0;
+                is_done <= 0;
+            end
+            WRITE: begin
+                state <= IDLE;
+                is_done <= 1;
+            end
+            READ: begin
+                state <= IDLE;
+                is_done <= 1;
+            end
+            default: begin end
+        endcase
+
+        if (trigger) begin
+            mem_addr <= addr;
+            if (write) begin
+                writing <= 1;
+                state <= WRITE;
+                write_in <= writevalue;
+            end
+            else begin
+                writing <= 0;
+                state <= READ;
+            end
+        end
+        /*case (state)
             IDLE: begin
                 is_done <= 0;
                 if (trigger) begin
@@ -63,9 +100,10 @@ module arrayblock #(
             default: begin
                 state <= IDLE;
             end
-        endcase
+        endcase*/
     end
 
+    assign readvalue[31:0] = read_out[31:0];
     assign done = is_done;
 
 endmodule
